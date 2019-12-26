0fb3bfd4160c577a55992c7f7e6d44a4e90f4f01
Bousine <asif21hasan@gmail.com>
2019-12-25 22:54:46 -0500

    Added a route handler

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 app.use('/', require('./routes/index'))
 app.use('/api/url', require('./routes/url'))
 
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname+'client/build/index.html'))
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
