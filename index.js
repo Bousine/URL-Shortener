e2a33c6f71850487df32cad649b37d72583f088d
Bousine <asif21hasan@gmail.com>
2019-12-25 20:13:48 -0500

    Added back db.js

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const app = express()
 connectDB()
 
 app.use(express.json({extended: false}))
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use('/', require('./routes/index'))
 app.use('/api/url', require('./routes/url'))
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
