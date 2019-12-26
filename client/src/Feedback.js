0273a3bae0c6ca3594b6a9d67daebed4ccba5ab2
Bousine <asif21hasan@gmail.com>
2019-12-26 08:57:03 -0500

    Added new production build

--- client/src/Feedback.js
+++ client/src/Feedback.js
@@ -7,7 +7,7 @@ function Feedback(props){
        <p style={{color: 'blue'}}>Link is already shortened</p> : null}
      {!props.attributes.valid ?
        <p style={{color: 'red'}}>Invalid URL provided. Remember to include
-        http(s):// at the beginning of the URL</p> : null}
+        http(s):// at the beginning of the URL.</p> : null}
      {props.attributes.success && !props.attributes.alreadyShort ?
        <p style={{color: 'green'}}>Copy the shortened link above! :)</p> : null
      }
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 app.use('/', require('./routes/index'))
 app.use('/api/url', require('./routes/url'))
 
-app.get('*', (req,res)=>{
+app.get('/*', (req,res)=>{
   res.sendFile(path.join(__dirname+'client/build/index.html'))
 })
 
